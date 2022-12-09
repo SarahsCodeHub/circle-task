3922e2c6a216ffb05d7514cc6d0a09e98c7aad12
Sarah Michaelsen <sarah.michaelsen@yahoo.com>
2022-12-09 15:01:39 +0100

    use coords in circle component

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,10 +1,8 @@
 <template>
     <div id="canvas">
         <h2>Click inside the box &nbsp;&darr;</h2>
-        <div class="box box--dashed" @click="drawCircle">
-            <div v-for="coords in activeCoords" :key="coords.id">
-                <VCircle>{{ coords }}</VCircle>
-            </div>
+        <div class="box box--dashed" @click="drawCircle" style="position: relative">
+            <VCircle v-for="([x, y], key) in activeCoords" :key="key" :xCoord="x" :yCoord="y"></VCircle>
         </div>
     </div>
 </template>
@@ -33,7 +31,6 @@ export default {
 
 <style lang="css" scoped>
 .box {
-    padding: 1.5rem;
     margin: 2rem 4rem;
     min-height: 60vh;
 }
--- src/components/Circle.vue
+++ src/components/Circle.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-
+    <div style="position: absolute; font-size: 2rem; cursor: crosshair" :title="`[x: ${xCoord}, y:${yCoord}]`"
+        :style="{ left: xCoord + 'px', top: yCoord + 'px' }">
+        &#8859;
     </div>
 </template>
 
 <script>
 export default {
-    name: "VCircle"
+    name: "VCircle",
+    props: {
+        xCoord: {
+            type: Number
+        },
+        yCoord: {
+            type: Number
+        }
+    },
 }
 </script>
 

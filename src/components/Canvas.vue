97e90059e13fd18dff1f510ee092dc71e172f659
Sarah Michaelsen <sarah.michaelsen@yahoo.com>
2022-12-09 15:43:21 +0100

    WIP redo circles

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="canvas">
         <h2>Click inside the box &nbsp;&darr;</h2>
-        <div class="box box--dashed" @click="drawCircle" @keydown.stop.prevent="undoLastCircle" tabindex="0"
+        <div class="box box--dashed" @click="drawCircle" @keydown.stop.prevent="redoOrUndoLastCircle" tabindex="0"
             style="position: relative">
             <VCircle v-for="([x, y], key) in activeCoords" :key="key" :xCoord="x" :yCoord="y"></VCircle>
         </div>
@@ -30,11 +30,30 @@ export default {
             const clientY = event.clientY;
             this.activeCoords.push([clientX - boxLeft, clientY - boxTop])
         },
-        undoLastCircle(event) {
-            if (event.keyCode == 90 && event.ctrlKey && this.activeCoords.length > 0) {
+        undoLastCircle() {
+            if (this.activeCoords.length > 0) {
                 const newStashedCircle = this.activeCoords[this.activeCoords.length - 1]
                 this.activeCoords.shift();
                 this.stashedCoords.push(newStashedCircle);
+            }
+        },
+        redoLastCircle() {
+            if (this.stashedCoords.length > 0) {
+                const redoneCircle = this.stashedCoords[this.stashedCoords.length - 1]
+                this.stashedCoords.shift();
+                this.activeCoords.push(redoneCircle);
+            }
+        },
+        redoOrUndoLastCircle(event) {
+            console.log(event.shiftKey && event.ctrlKey)
+            if (!event.ctrlKey) {
+                return
+            } else if (event.shiftKey && event.ctrlKey && event.keyCode == 90) {
+                this.redoLastCircle()
+            } else if (event.ctrlKey && event.keyCode === 90) {
+                this.undoLastCircle()
+            } else {
+                return
             }
         }
     },
